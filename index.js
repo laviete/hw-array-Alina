e0be247911aa98e4578c2cffc8d5858e6918bc3d
Alina <135998117+laviete@users.noreply.github.com>
2023-07-20 00:22:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function checkValueInArray(array, key) {
   return array.includes(key);
 }
-function checkDuplicateNum(array) {
+function checkTwoDuplicateNumInArray(array) {
   for (let i = 0; i < array.length - 1; i++) {
     if (array[i] === array[i + 1]) {
       return true;
@@ -17,11 +17,11 @@ const arr2 = [1, 2, 3, 4, 5];
 const key2 = 7;
 console.log(checkValueInArray(arr2, key2)); //false(такого ключа немає в масиві)
 
-const arr3 = [9, 9, 7, 8, 6, 5];
-console.log(checkDuplicateNum(arr3)); //true
+const arr3 = [9, 8, 7, 6, 5, 5];
+console.log(checkTwoDuplicateNumInArray(arr3)); //true
 
 const arr4 = [9, 8, 7, 8, 6, 5]; 
-console.log(checkDuplicateNum(arr4)); //false(повторювані числа не задані поспіль)
+console.log(checkTwoDuplicateNumInArray(arr4)); //false(повторювані числа не задані поспіль)
 
 const arr5= [];
-console.log(checkDuplicateNum(arr5)); //false(пустий масив)
+console.log(checkTwoDuplicateNumInArray(arr5)); //false(пустий масив)
